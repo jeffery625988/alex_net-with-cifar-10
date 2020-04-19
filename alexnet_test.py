981b5c549a2a72d6c719b0a41b9a2d1943313304
jeffery <jeffery625988@gmail.com>
2020-04-19 12:56:02 +0800

    add alexnet train and test

--- alexnet_test.py
+++ alexnet_test.py
@@ -1,1 +1,91 @@
 
+import tensorflow as tf
+from sklearn.preprocessing  import MinMaxScaler
+import time
+import alexnet_model
+import alexnet_train
+import os
+import numpy as np
+
+data_dir = "D:\data/"
+extractor_folder = 'cifar-10-batches-bin'
+def encode(y,k):   #y is label k is how many class
+    one_hot =  np.zeros((y.shape[0],k))
+    for idx, val in enumerate(y):
+        one_hot[idx,val] = 1.0
+    return one_hot
+
+def load_testdata(): #n=1,2..5,data_batch_1.bin ~data_batch_5.bin
+    test_path = os.path.join(data_dir,extractor_folder,'test_batch.bin')
+    with open(test_path,'rb') as testpath:
+        test_img = np.fromfile(testpath,dtype=np.uint8)
+    return test_img
+
+#test
+def test(x_test_image,y_test_label):
+    with tf.Graph().as_default() as g:
+
+
+        x_ = tf.placeholder(tf.float32,[None,alexnet_train.n_input])
+        x = tf.reshape(x_,shape=[-1,32,32,3])
+        y = tf.placeholder(tf.float32,[None,10])
+
+        #construct
+        pred ,parameter= alexnet_model.model(x,1)   #dropout = 1
+
+
+        #evaluate
+        correct_pred = tf.equal(tf.arg_max(pred,1),tf.arg_max(y,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))
+
+        test_batch_len = int(x_test_image.shape[0]/200)
+        test_acc = []
+
+        test_xs = np.reshape(x_test_image,(x_test_image.shape[0],32,32,3))
+
+        batch_size = 200
+
+
+        saver=tf.train.Saver()
+        # restore all the variable
+        with tf.Session() as sess :
+            saver.restore(sess,"./alexnet/alexnet_cifar_model")
+
+            for i in range(test_batch_len):
+                _acc = sess.run(accuracy,feed_dict={x:test_xs[batch_size*i:batch_size*i+batch_size],y: y_test_label[batch_size*i:batch_size*i+batch_size]})
+                test_acc.append(_acc)
+                print ("Test batch ",i,":Testing Accuracy:",_acc)
+
+            t_acc = tf.reduce_mean(tf.cast(test_acc,tf.float32))
+            print("Average Testing Accuracy=",sess.run(t_acc))
+            return
+def main(argv=None):
+    x_test_images = load_testdata()
+    x_test_images = x_test_images.reshape(-1, 3073)  # reshape to (10000,3073)
+    tempTest = x_test_images.copy()
+
+    x_test_images = np.delete(x_test_images, 0, 1)
+    x_test_images = x_test_images.reshape(-1, 3, 32, 32)
+    x_test_images = x_test_images.transpose([0, 2, 3, 1])
+    x_test_images = x_test_images.reshape(-1, 3072)
+
+    tempTest = np.hsplit(tempTest, 3073)
+    x_test_label = np.asarray(tempTest[0])
+    x_test_label = x_test_label.reshape([10000, ])
+
+
+    # normalization
+    mms = MinMaxScaler()
+    x_test_images = mms.fit_transform(x_test_images)
+
+    #encode label
+    x_test_label = encode(x_test_label, 10)
+
+    print("X_test_image.shape=", x_test_images.shape)
+    print("X_test_label.shape=", x_test_label.shape)
+    print(x_test_label[0:50])
+
+    test(x_test_images,x_test_label)
+
+if __name__ == '__main__':
+	main()
--- alexnet_train.py
+++ alexnet_train.py
@@ -1,1 +1,150 @@
 
+import  tensorflow as tf
+import os
+import struct
+import numpy as np
+import  cv2,csv
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import alexnet_model
+
+
+
+data_dir = "D:\data/"
+extractor_folder = 'cifar-10-batches-bin'
+def encode_labels(y,k):   #y is label k is how many class
+    one_hot =  np.zeros((y.shape[0],k))
+    for idx, val in enumerate(y):
+        one_hot[idx,val] = 1.0
+    return one_hot
+def load_train_data(n): #n=1,2..5,data_batch_1.bin ~data_batch_5.bin
+    image_path = os.path.join(data_dir,extractor_folder,'data_batch_{}.bin'.format(n))
+    with open(image_path,'rb') as imgpath:
+        images = np.fromfile(imgpath,dtype=np.uint8)
+    return images
+def load_test_data(): #n=1,2..5,data_batch_1.bin ~data_batch_5.bin
+    test_path = os.path.join(data_dir,extractor_folder,'test_batch.bin')
+    with open(test_path,'rb') as testpath:
+        test_img = np.fromfile(testpath,dtype=np.uint8)
+    return test_img
+
+#parameter
+MODEL_SAVE_PATH ='./alexnet/'
+MODEL_NAME = "alexnet_cifar_model"
+learning_rate = 0.001
+BATCH_SIZE = 200
+display_step = 100
+TRAIN_STEP = 10000
+#network parameter
+n_input = 3072 #cifar size
+n_class = 10   #classes
+dropout = 0.5
+
+def train(x_train,y_train_label):
+    shuffle = True
+    batch_idx = 0
+    batch_len = int(x_train.shape[0]/BATCH_SIZE)
+    train_accuracy = []
+    train_idx = np.random.permutation(batch_len) #打散batch 500 group e.g[2,50,36,152,....]
+
+    # tf image input
+    x_ = tf.placeholder(tf.float32,shape=[None,n_input])
+    y_ = tf.placeholder(tf.float32,shape=[None,n_class])
+    keep_prob = tf.placeholder(tf.float32)
+    x = tf.reshape(x_,shape=[-1 ,32 ,32,3 ])
+
+    #construct model
+    _train ,parameter = alexnet_model.model(x,keep_prob)
+
+
+    #define optimizer loss function
+    cost1 =tf.add_to_collection('losses',tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=_train,labels=y_)))
+    cost2 =tf.add_to_collection('losses',tf.multiply(tf.nn.l2_loss(t=parameter[0],name=None),0.004))
+    total_loss = tf.add_n(tf.get_collection('losses'))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(total_loss)
+
+    #evaluate model
+    correct_pred = tf.equal(tf.arg_max(_train,1),tf.arg_max(y_,1)) #找出predict 出來機率最大和label相比 bool
+    accuracy = tf.reduce_mean(tf.cast(correct_pred,tf.float32))#tf.cast  cast tensor to a new type
+
+    #save model
+    saver = tf.train.Saver()
+    init = tf.global_variables_initializer()
+
+    #start
+    with tf.Session() as sess:
+        sess.run(init)
+        step = 1
+        print("Starting training")
+        #keep training until reach max iteration
+        while step <TRAIN_STEP:
+            if shuffle == True:    #洗牌ㄇ 打散batch順序
+                batch_shuffle_idx = train_idx[batch_idx]  #train_idx 打散500group 從其第一個開始
+                batch_xs = x_train[batch_shuffle_idx*BATCH_SIZE:batch_shuffle_idx*BATCH_SIZE+BATCH_SIZE]   #from 0*200:0*200+200 (first batch)
+                batch_ys = y_train_label[batch_shuffle_idx*BATCH_SIZE:batch_shuffle_idx*BATCH_SIZE+BATCH_SIZE]
+            else:
+                batch_xs = x_train[batch_idx * BATCH_SIZE:batch_idx * BATCH_SIZE + BATCH_SIZE]  # from 0*200:0*200+200 (first batch)
+                batch_ys = y_train_label[batch_idx * BATCH_SIZE:batch_idx * BATCH_SIZE + BATCH_SIZE]
+            if batch_idx < batch_len:
+                batch_idx += 1
+                if batch_idx == batch_len:
+                        batch_idx = 0
+            else:
+                    batch_idx = 0
+            reshaped_xs = np.reshape(batch_xs,(BATCH_SIZE,32,32,3))
+            #optimize(backprop)
+            sess.run(optimizer, feed_dict={x: reshaped_xs, y_: batch_ys,keep_prob: dropout})
+
+            # Calculate batch loss and accuracy
+            loss, acc = sess.run([total_loss, accuracy], feed_dict={x: reshaped_xs, y_: batch_ys,keep_prob: 1.})
+            train_accuracy.append(acc)
+            if step % display_step == 0:
+                print("Step:"+str(step)+",Mini_batch Loss="+"{:.6f}".format(loss)+",Training ACC="+"{:.5f}".format(acc))
+            step +=1
+        print("Optimization Finished!")
+        print("Save model...")
+        # saver.save(sess, "./alexnet/alexnet_model")
+        saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME))
+def main(argv=None):
+
+    ##Load Cifar-10 train image and label
+    X_train_image1 = load_train_data(1)  # load data_batch_1.bin
+    X_train_image2 = load_train_data(2)  # load data_batch_2.bin
+    X_train_image3 = load_train_data(3)  # load data_batch_3.bin
+    X_train_image4 = load_train_data(4)  # load data_batch_4.bin
+    X_train_image5 = load_train_data(5)  # load data_batch_5.bin
+    print(X_train_image1.shape)
+
+    X_train_image = np.concatenate((X_train_image1, X_train_image2, X_train_image3, X_train_image4, X_train_image5),
+                                   axis=0)
+    print(X_train_image.shape)
+
+    # reshape to (50000,3073)
+    # in one Row ,the 1st byte is the label,other 3072byte =1024 Red +1024 green +1024 blue ch data
+    X_train_image = X_train_image.reshape(-1, 3073)
+    tempA = X_train_image.copy()
+    X_train_image = np.delete(X_train_image, 0, 1)  # delete 1st column data
+    X_train_image = X_train_image.reshape(-1, 3, 32, 32)  # (50000,3,32,32)
+    X_train_image = X_train_image.transpose([0, 2, 3, 1])  # transfer to (10000,32,32,3)
+    X_train_image = X_train_image.reshape(-1, 3072)  # (50000,3,32,32)
+
+    # split to 3073 col,the first column is the label.
+    tempA = np.hsplit(tempA, 3073)
+    X_train_label = np.asarray(tempA[0])
+    X_train_label = X_train_label.reshape([50000, ])
+
+    print(X_train_image.shape)
+    print(X_train_label.shape)
+    print(X_train_label[0:50])
+
+    mms = MinMaxScaler()
+    X_train_image = mms.fit_transform(X_train_image)
+
+    X_train_label = encode_labels(X_train_label, 10)
+    print("y_train_lable.shape=", X_train_label.shape)
+
+    train(X_train_image, X_train_label)
+
+if __name__ == '__main__':
+    main()
+
